dc2c12cdea6ca017f7803861adeaaa0cfcc4812b
Gabe Tao <tao.gabe@gmail.com>
2024-01-19 13:33:13 -0600

    Update 2.2.1.4_configure_ntp.sh

--- bin/hardening/2.2.1.4_configure_ntp.sh
+++ bin/hardening/2.2.1.4_configure_ntp.sh
@@ -43,30 +43,30 @@ audit() {
 
 # This function will be called if the script status is on enabled mode
 apply() {
-    is_pkg_installed "$PACKAGE"
-    if [ "$FNRET" = 0 ]; then
-        ok "$PACKAGE is installed"
-    else
-        crit "$PACKAGE is absent, installing it"
-        apt_install "$PACKAGE"
-        info "Checking $PACKAGE configuration"
-    fi
-    does_pattern_exist_in_file "$NTP_CONF_FILE" "$NTP_CONF_DEFAULT_PATTERN"
-    if [ "$FNRET" != 0 ]; then
-        warn "$NTP_CONF_DEFAULT_PATTERN not found in $NTP_CONF_FILE, adding it"
-        backup_file "$NTP_CONF_FILE"
-        add_end_of_file "$NTP_CONF_FILE" "restrict -4 default kod notrap nomodify nopeer noquery"
-    else
-        ok "$NTP_CONF_DEFAULT_PATTERN found in $NTP_CONF_FILE"
-    fi
-    does_pattern_exist_in_file "$NTP_INIT_FILE" "^$NTP_INIT_PATTERN"
-    if [ "$FNRET" != 0 ]; then
-        warn "$NTP_INIT_PATTERN not found in $NTP_INIT_FILE, adding it"
-        backup_file "$NTP_INIT_FILE"
-        add_line_file_before_pattern "$NTP_INIT_FILE" "$NTP_INIT_PATTERN" "^UGID"
-    else
-        ok "$NTP_INIT_PATTERN found in $NTP_INIT_FILE"
-    fi
+    # is_pkg_installed "$PACKAGE"
+    # if [ "$FNRET" = 0 ]; then
+    #     ok "$PACKAGE is installed"
+    # else
+    #     crit "$PACKAGE is absent, installing it"
+    #     apt_install "$PACKAGE"
+    #     info "Checking $PACKAGE configuration"
+    # fi
+    # does_pattern_exist_in_file "$NTP_CONF_FILE" "$NTP_CONF_DEFAULT_PATTERN"
+    # if [ "$FNRET" != 0 ]; then
+    #     warn "$NTP_CONF_DEFAULT_PATTERN not found in $NTP_CONF_FILE, adding it"
+    #     backup_file "$NTP_CONF_FILE"
+    #     add_end_of_file "$NTP_CONF_FILE" "restrict -4 default kod notrap nomodify nopeer noquery"
+    # else
+    #     ok "$NTP_CONF_DEFAULT_PATTERN found in $NTP_CONF_FILE"
+    # fi
+    # does_pattern_exist_in_file "$NTP_INIT_FILE" "^$NTP_INIT_PATTERN"
+    # if [ "$FNRET" != 0 ]; then
+    #     warn "$NTP_INIT_PATTERN not found in $NTP_INIT_FILE, adding it"
+    #     backup_file "$NTP_INIT_FILE"
+    #     add_line_file_before_pattern "$NTP_INIT_FILE" "$NTP_INIT_PATTERN" "^UGID"
+    # else
+    #     ok "$NTP_INIT_PATTERN found in $NTP_INIT_FILE"
+    # fi
 }
 
 # This function will check config parameters required
